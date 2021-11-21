7f522276db11846d97a4b5462c2ea7ebefe8916c
jaradamiak <jaradamiak@gmail.com>
2021-11-21 13:41:27 +0200

    commit 2021/11/21

--- app.py
+++ app.py
@@ -23,3 +23,12 @@ def user(name):
     return render_template('user.html', user_name=name)
     return "<h1>Hello {}</h1>".format(name)
 
+# invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# internal server error    
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
