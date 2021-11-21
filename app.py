5f053ebaf78a11f9fae0986fefdf153d287d965d
jaradamiak <jaradamiak@gmail.com>
2021-11-21 15:04:35 +0200

    2021/22/21 v2

--- app.py
+++ app.py
@@ -3,10 +3,18 @@
 # export FLASK_APP=app.py
 # pip freeze > requirements.txt
 from flask import Flask, render_template
-from flask_wtf import Form
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create route
 @app.route('/')
@@ -32,3 +40,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
+
+
+
